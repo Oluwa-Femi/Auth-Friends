b16bc1fed2b96471059881e07a9768b8c7989a38
unknown <oluwatolablessing@gmail.com>
2019-10-16 12:16:17 +0100

    Delete friend

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -5,25 +5,36 @@ import Card from './Card';
 
 const Friends = (props) => {
     const [friendsList, setFriendsList] = useState([]);
-    useEffect(() => {
+    const getFriends = () => {
         axiosAuth().get('http://localhost:5000/api/friends')
             .then(res => {
                 setFriendsList(res.data);
             })
             .catch(err => console.log(err.response));
-    }, []);
+        };
+    useEffect(() => {
+        getFriends();
+        }, []);
 
     const addFriend = friend => {
         axiosAuth().post('http://localhost:5000/api/friends', friend)
         .then(res => setFriendsList(res.data))
         .catch(err => console.log(err.response));
     };
+    const deleteFriend = id => {
+        // console.log(id);
+        axiosAuth().delete(`http://localhost:5000/api/friends/${id}`)
+        .then(res => setFriendsList(res.data))
+            .catch(err => console.log(err.response));
+    };
     return (
         <div>
             <h2>Friends</h2>
             <FriendsList submitFriend={addFriend}/>
             {friendsList.map(friend => {
-                return <Card key={friend.id} friend={friend}/>;
+                return <Card key={friend.id}
+                friend={friend}
+                deleteFriend={deleteFriend}/>;
             })}
         </div>
     );
